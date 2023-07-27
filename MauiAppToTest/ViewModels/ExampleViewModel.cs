8358f7ad9bd90fc28fc27766ec2dfdc97bafe962
Tom Soderling <18268845+TomSoderling@users.noreply.github.com>
2023-07-27 15:02:20 -0500

    Fix formatting

--- MauiAppToTest/ViewModels/ExampleViewModel.cs
+++ MauiAppToTest/ViewModels/ExampleViewModel.cs
@@ -7,15 +7,15 @@ namespace MauiAppToTest.ViewModels
     /// This is an example ViewModel to help illustrate unit testing best practices. See ExampleViewModelTests
     /// </summary>
     public class ExampleViewModel
-	{
-		private readonly IUIThreadInvocation uIThreadInvocation;
+    {
+	private readonly IUIThreadInvocation uIThreadInvocation;
 
         public ExampleViewModel(IUIThreadInvocation uIThreadInvocation) // this service is being injected here just for illustration purposes
         {
-			this.uIThreadInvocation = uIThreadInvocation;
+	    this.uIThreadInvocation = uIThreadInvocation;
         }
 
-		public string PageTitle { get; set; }
+	public string PageTitle { get; set; }
 
         public int DoSomeWork()
         {
@@ -29,4 +29,3 @@ namespace MauiAppToTest.ViewModels
         }
     }
 }
-
